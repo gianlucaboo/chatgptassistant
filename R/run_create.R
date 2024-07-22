cf3a1386b90af50410b0e80aa14641d5a5face55
Gianluca Boo <gianluca.boo@gmail.com>
2024-07-22 13:00:26 +0100

    run create update

--- R/run_create.R
+++ R/run_create.R
@@ -13,7 +13,7 @@ run_create <- function(thread_id, assistant_id, api_key) {
   httr2::request(paste0("https://api.openai.com/v1/threads/", thread_id, "/runs")) |>
     httr2::req_auth_bearer_token(token=api_key) |>
     httr2::req_headers("Content-Type"="application/json", "OpenAI-Beta"="assistants=v2") |>
-    httr2::req_body_json(data=list(assistant_id=asst_id)) |>
+    httr2::req_body_json(data=list(assistant_id=assistant_id)) |>
     httr2::req_perform() |>
     httr2::resp_body_json()
 }
