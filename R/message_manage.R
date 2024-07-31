d9bb08833aba31444695e28e6e8f0cc0f4c00f2f
Gianluca Boo <gianluca.boo@gmail.com>
2024-07-31 15:04:32 +0100

    message manage

--- R/message_manage.R
+++ R/message_manage.R
@@ -8,6 +8,9 @@
 #'
 #' @import httr2
 #' @importFrom utils menu
+#' @importFrom data.table rbindlist
+#' @importFrom dplyr pull
+#' @importFrom purrr walk
 #' @importFrom chatgptassistant message_list
 #' @importFrom chatgptassistant message_delete
 #' @examples
@@ -16,16 +19,21 @@
 message_manage <- function(thread_id, api_key){
   message_delete <- utils::menu(c("Yes", "No"), title = "Do you want to delete all the messages?")
   if (message_delete == 1) {
-    message_n <- message_list(thread_id=thread_id,
-                              api_key=api_key)[["data"]] |> length()
+    message_n <-
+      message_list(thread_id=thread_id,
+                   api_key=api_key)[["data"]] |>
+      length()
 
-    while(length(message_n)>0) {
-      delete_message <-  message_list(thread_id=thread_id,
-                                      api_key=api_key)[["data"]] |>
-        rbindlist() |>
-        pull(id) |>
-        walk(function(message_id) {
-          message_delete(thread_id=thread$id, message_id=message_id, api_key=api_key)
+    while(message_n>0) {
+      delete_message <-
+        message_list(thread_id=thread_id,
+                     api_key=api_key)[["data"]] |>
+        data.table::rbindlist() |>
+        dplyr::pull(id) |>
+        purrr::walk(function(message_id) {
+          message_delete(thread_id=thread_id,
+                         message_id=message_id,
+                         api_key=api_key)
         })
       message_n <-
         message_list(thread_id=thread_id,
@@ -33,8 +41,9 @@ message_manage <- function(thread_id, api_key){
     }
     print("All messages in the thread have been deleted")
   } else {
-    message_n <- message_list(thread_id=thread_id,
-                              api_key=api_key)[["data"]] |> length()
+    message_n <-
+      message_list(thread_id=thread_id,
+                   api_key=api_key)[["data"]] |> length()
     print(paste0("The thread contains",  message_n, " message(s)"))
   }
 }
